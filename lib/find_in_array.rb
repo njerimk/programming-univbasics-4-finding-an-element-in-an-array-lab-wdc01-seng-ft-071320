347e3db38be2dd917dc38dafff6c3a22f24ea1ba
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-04 21:13:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count=0
   while count < array.length do
-    puts array[count]== value_to_find 
+    array[count]== value_to_find
+    puts value_to_find.index
     count+= 1
  end
 end
